2207b8cf94f14ef07248389ba25a97ec833efee3
syachi <1040899365@qq.com>
2015-12-20 22:18:20 +0800

    写了一下基于数子的胜负判断 还需要实现一下棋串 胜负判断也需要明天研究一下

--- board.cpp
+++ board.cpp
@@ -169,7 +169,7 @@ void board::showboard(){
 std::set<int>* board::getemptycells(){
 	//std::vector<int> result;
 	//result.clear();
-	//std::set<int>::iterator it;  
+	//std::set<int>::iterator it;
 	//for(it=emptycells.begin();it!=emptycells.end();it++){
 	//	result.push_back(*it);
 	//}
@@ -206,4 +206,51 @@ bool board::play(bool& player,int coordx, int coordy){
 	}
 	currentplayer = !currentplayer;
 	return true;
+}
+
+cell board::get_cell(int i, int j){
+    return goban[i][j].c;
+}
+
+void board::compute_final_status(void){
+    for(int pos = 0; pos < SIZE*SIZE; ++pos)
+        final_status[pos] = UNKNOWN;
+
+    for(int i = 1; i < SIZE+1; ++i){
+        for(int j = 1; j < SIZE+1; ++j){
+            if(get_cell(i, j) == empty){
+                for(int k = 0; k < 4; k++){
+                    int ai = i + offset_x[k];
+                    int aj = j + offset_y[k];
+                    if(get_cell(ai, aj) == border)
+                        continue;
+                    pos = POS(ai,aj);
+                    if (final_status[pos] == UNKNOWN) {
+                        if (get_board(ai, aj) != EMPTY) {
+                            if (has_additional_liberty(ai, aj, i, j))
+                                set_final_status_string(pos, ALIVE);
+                            else
+                                set_final_status_string(pos, DEAD);
+                        }
+                    }
+                    if (final_status[POS(i, j)] == UNKNOWN) {
+                        if ((final_status[pos] == ALIVE) ^ (get_board(ai, aj) == WHITE))
+                            final_status[POS(i, j)] = BLACK_TERRITORY;
+                        else
+                            final_status[POS(i, j)] = WHITE_TERRITORY;
+                    }
+                }
+            }
+        }
+    }
+}
+
+void black_raw(){
+    int black = 0;
+    for(int pos = 0; pos < SIZE*SIZE; ++pos){
+        if(final_status[pos] = WHITE_TERRITORY || final_status[pos] = UNKNOWN) continue;
+        if(final_status[pos] = BLACK_TERRITORY){black++; continue;}
+        if((final_status[pos] == ALIVE) ^ (get_cell(I(pos), J(pos)) == WHITE)) black++;
+    }
+    return black;
 }
--- board.h
+++ board.h
@@ -5,8 +5,22 @@
 #include <set>
 #define SIZE 13
 #define PROTOCOL_VERSION "2"
-#define NAME "Neuroart" 
+#define NAME "Neuroart"
 #define VERSION "0.01"
+
+#define DEAD 0
+#define ALIVE 1
+#define SEKI 2
+#define WHITE_TERRITORY 3
+#define BLACK_TERRITORY 4
+#define UNKNOWN 5
+
+#define POS(i, j) ((i-1) * board_size + (j-1))
+#define I(pos) (((pos) / board_size) + 1)
+#define J(pos) (((pos) % board_size) + 1)
+
+static int offset_x[4] = {-1, 1, 0, 0};
+static int offset_y[4] = {0, 0, -1, 1};
 
 enum cell{
 	empty = 0, //0
@@ -36,6 +50,8 @@ private:
 	int ko_i;
 	int ko_j;
 
+	int final_status[SIZE][SIZE];
+
 	void place(bool player, kaku*k);
 	bool deathtest(kaku* k);
 	void kill(kaku* k);
@@ -51,6 +67,7 @@ public:
 	void get_known_command(); //未实现，返回类型未确定
 	void quit(); //未实现，返回类型未确定
 	int get_boardsize(){return board_size;}
+	cell get_cell(int i, int j);
 	void clear_board();
 	void get_komi(); //未实现，返回类型未确定
 	void get_fixed_handicap(); //未实现，返回类型未确定
@@ -59,6 +76,11 @@ public:
 	void genmove(); //未实现，返回类型未确定
 	void final_score(); //未实现，返回类型未确定
 	void final_status_list(); //未实现，返回类型未确定
+	void compute_final_status();
+	void set_final_status_string(int pos, int status);//需要棋串
+	int has_additional_liberty(int i, int j, int libi, int libj)
+        //需要棋串，(i,j)所在的棋串除(libi,libj)外是否还有其他的气
+    int black_raw();//贴目前黑的总子数
 	void showboard();
 	bool getcurrentplayer(){return currentplayer;}
 	std::set<int>* getemptycells();
