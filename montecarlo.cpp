89531792c0040e3f4b2a55eba601811cc51d41da
Espressomilk <jack.shi2013@gmail.com>
2015-12-21 20:07:31 +0800

    make some changes for MC
    now MC will terminate in two conditions. 1 after 300 valid moves; 2
    continuously 50 moves invalid

--- montecarlo.cpp
+++ montecarlo.cpp
@@ -17,12 +17,21 @@ void montecarlo::getInitBoard(board inBoard) {
 
 void montecarlo::run() {
 	srand((unsigned)time(NULL));
-	while (!currentBoard.finished()) {
+	int step = 0;
+	int fault = 0;
+	while (step <= 300) {
 		vector<int> legalPos = currentBoard.getemptycells();
 		int randomNumber = rand() * legalPos.length() / (MAXINT + 1);
 		int coordX = legalPos[randomNumber] / SIZE;
 		int coordY = legalPos[randomNumber] % SIZE;
-		currentBoard.play(currentBoard.currentPlayer, coordX, coordY);
+		if(currentBoard.play(currentBoard.currentPlayer, coordX, coordY)) {
+			step++;
+			fault = 0;
+		}
+		else {
+			fault++;
+		}
+		if(fault >= 50) break;
 	}
 	winner = currentBoard.winner;
 }
